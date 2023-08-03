2e373ef625bd0102f12071e6106d1e3768aac129
Luis Martinez <luismartinezwebdev@gmail.com>
2023-08-03 09:23:41 +0200

    chore: first commit

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import "./index.css";
+
+const element = {
+  type: "h1",
+  props: {
+    title: "foo",
+    children: "Hello",
+  },
+};
+const container = document.getElementById("root") as HTMLElement;
+
+const node = document.createElement(element.type);
+node["title"] = element.props.title;
+
+const text = document.createTextNode("");
+text["nodeValue"] = element.props.children;
+
+node.appendChild(text);
+container.appendChild(node);
