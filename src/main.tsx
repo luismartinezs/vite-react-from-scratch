f16eb513e10f6e079c63a4129f22864a888265a4
Luis Martinez <luismartinezwebdev@gmail.com>
2023-08-09 18:08:39 +0200

    feat: add some styles

--- src/main.tsx
+++ src/main.tsx
@@ -10,8 +10,16 @@ const Zeact = {
 /** @jsx Zeact.createElement */
 function Counter() {
   const [state, setState] = Zeact.useState(1);
-  return <h1 onClick={() => setState((c) => c + 1)}>Count: {state}</h1>;
+  return (
+    <div>
+      <h1>React from scratch</h1>
+      <button className="button" onClick={() => setState((c: number) => c + 1)}>
+        Count: {state}
+      </button>
+    </div>
+  );
 }
+
 const element = <Counter />;
 const container = document.getElementById("root");
 Zeact.render(element, container);
