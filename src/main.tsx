2ec4ba82fb391140c56b8a5d93e7b0bf17fbf902
Luis Martinez <luismartinezwebdev@gmail.com>
2023-08-10 18:28:43 +0200

    feat: bloody hell...

--- src/main.tsx
+++ src/main.tsx
@@ -1,36 +1,13 @@
-import { createElement, render, useState } from "./zeact";
+import { createElement, render } from "./zeact";
 import "./index.css";
 
 const Zeact = {
   createElement,
   render,
-  useState,
 };
 
-const todos = ["do chore 1", "do chore 2"];
-
 /** @jsx Zeact.createElement */
-function Counter() {
-  const [state, setState] = Zeact.useState(1);
-  return (
-    <div>
-      <h1
-        style={{
-          textTransform: "uppercase",
-        }}
-      >
-        React from scratch
-      </h1>
-      <button className="button" onClick={() => setState((c: number) => c + 1)}>
-        Count: {state}
-      </button>
-      {todos.map((todo) => (
-        <div>{todo}</div>
-      ))}
-    </div>
-  );
-}
+const element = <h1 id="foo">Hello world</h1>;
 
-const element = <Counter />;
 const container = document.getElementById("root");
 Zeact.render(element, container);
